b6b09139716990967f843d53d07fac26401abf54
Bryan Clover <bclover09@gmail.com>
2019-01-23 13:31:19 -0500

    move JSON file to proper location & render some categories

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,29 @@ class App extends Component {
     };
     axios.get('http://localhost:3000/music.json')
         .then(res => {
+          // console.log('res:', res);
           this.setState({ data: res.data });
         });
   }
 
   render() {
+    const music = this.state.data;
+    console.log('music:', music);
+    const categories = music.map((category) => {
+      return <li>{category.type.toUpperCase()}</li>;
+    });
     return (
       <div className="App">
         <section>
           <div>
-            <p>Given the following data, let's practice parsing values.</p>
-            <p className="json-data">
-              {this.state.data}
-            </p>
+            <p>Given some JSON data about music, let's practice parsing values.</p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <ul className="categories">
+              {categories}
+            </ul>
           </div>
         </section>
       </div>
